994d5463c810defd28cc6df7fce79287988cd063
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2020-09-18 21:33:40 +0200

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTiime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repositorycs.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+    }
+}
---
+++ PartyInvites/Models/Repositorycs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public static class Repositorycs
+    {
+        private static List<Guestresponse> responses = new List<Guestresponse>();
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
