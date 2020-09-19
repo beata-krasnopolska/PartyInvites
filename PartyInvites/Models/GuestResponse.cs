6b630e548d2a343ac8fe9ec615e8b95f5fcc684d
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2020-09-19 23:17:38 +0200

    Add fields validation and styles on the main page

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please add your name and family name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please add an e-mial address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please add a phone number")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you attend")]
         public bool? WillAttend { get; set; }
     }
 }
